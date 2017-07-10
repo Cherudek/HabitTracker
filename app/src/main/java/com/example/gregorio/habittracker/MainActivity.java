26479ef9c0e980798699c17f63604433f11d1357
Gregorio <greg.massara@gmail.com>
2017-07-10 16:30:52 +0100

    called the functions in the MainActivity onCreate method now works! no errorrs ;)

--- app/src/main/java/com/example/gregorio/habittracker/MainActivity.java
+++ app/src/main/java/com/example/gregorio/habittracker/MainActivity.java
@@ -27,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mDbHelper = new HabitDbHelper(this);
+        insertPet();
+        readDatabase();
+        displayDatabaseInfo();
 
     }
 
